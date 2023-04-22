4f24fdda1f7a8bcb9ddae6989b977e70f9524f2c
Kushan Devarajegowda <kushande@buffalo.edu>
2023-04-22 13:40:13 -0400

    changes

--- app.py
+++ app.py
@@ -7,6 +7,24 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["pet_adoption"]  # Replace "your_database_name" with the desired name of your MongoDB database
 
 
+def find_top_10_dogs() -> List[Dict]:
+    collection = db["dog"]
+    dogs = collection.find().sort("name", 1).limit(10)
+    return list(dogs)
+
+
+def find_top_10_owners() -> List[Dict]:
+    collection = db["owner"]
+    owners = collection.find().sort("name", 1).limit(10)
+    return list(owners)
+
+
+def find_top_10_adoptions() -> List[Dict]:
+    collection = db["adoption"]
+    adoptions = collection.find().sort("adoption_date", -1).limit(10)
+    return list(adoptions)
+
+
 def find_top_5_dogs_of_breed(breed_name: str) -> List[Dict]:
     collection = db["dog"]
     dogs = collection.find({"breed.name": breed_name}).sort("name", 1).limit(5)
@@ -26,7 +44,7 @@ def find_top_5_dogs_by_owner(owner_id: str) -> List[Dict]:
 
 def update_dog_name(dog_id: object, new_name: str):
     collection = db["dog"]
-    result = collection.update_one({"_id": dog_id}, {"$set": {"name": new_name}})
+    result = collection.update_one({"_id": ObjectId(dog_id)}, {"$set": {"name": new_name}})
     return result.modified_count
 
 
@@ -38,7 +56,7 @@ def add_owner(owner: Dict):
 
 def delete_owner(owner_id: object):
     collection = db["owner"]
-    result = collection.delete_one({"_id": owner_id})
+    result = collection.delete_one({"_id": ObjectId(owner_id)})
     return result.deleted_count
 
 
@@ -52,9 +70,12 @@ if __name__ == "__main__":
         print("3. Update Dog name by id")
         print("4. Add a new owner")
         print("5. Delete owner")
+        print("6. Show top 10 records from the 'dog' collection")
+        print("7. Show top 10 records from the 'owner' collection")
+        print("8. Show top 10 records from the 'adoption' collection")
         print("Any other key. Exit")
 
-        choice = int(input("Enter your choice (1-5): "))
+        choice = int(input("Enter your choice (1-8): "))
 
         if choice == 1:
             breed_name = input("Enter the dog breed name: ")
@@ -101,5 +122,33 @@ if __name__ == "__main__":
             owner_id = input("Enter the owner ID: ")
             deleted_count = delete_owner(owner_id)
             print(f"Deleted {deleted_count} owner.")
+        elif choice == 6:
+            collection = db["dog"]
+            dogs = collection.find().limit(10)
+            print("\nTop 10 dogs:")
+            print("--------------")
+            for index, dog in enumerate(dogs):
+                print(
+                    f"{index + 1}. Name: {dog['name']}, Breed: {dog['breed']['name']}, Country: {dog['country']} (ID: {dog['_id']})")
+        elif choice == 7:
+            collection = db["owner"]
+            owners = collection.find().limit(10)
+            print("\nTop 10 owners:")
+            print("----------------")
+            for index, owner in enumerate(owners):
+                print(f"{index + 1}. {owner['name']} (ID: {owner['_id']})")
+        elif choice == 8:
+            collection = db["adoption"]
+            adoptions = collection.find().sort("adoption_date", -1).limit(10)
+            print("\nTop 10 adoptions:")
+            print("------------------")
+            for index, adoption in enumerate(adoptions):
+                dog_id = adoption["dog_id"]
+                owner_id = adoption["owner_id"]
+                dog_name = db["dog"].find_one({"_id": dog_id})["name"]
+                owner_name = db["owner"].find_one({"_id": owner_id})["name"]
+                adoption_date = adoption["adoption_date"].strftime("%d-%m-%Y")
+                print(
+                    f"{index + 1}. {dog_name} (ID: {dog_id}) - Adopted by {owner_name} (ID: {owner_id}) on {adoption_date}")
         else:
             print("Invalid choice. Please try again.")
